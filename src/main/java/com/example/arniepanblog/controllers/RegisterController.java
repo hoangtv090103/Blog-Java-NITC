aa9e87dcfae1ada5853252e8b2e6a1b4af1e009f
hoangtv090103 <102798323+hoangtv090103@users.noreply.github.com>
2023-02-26 04:37:19 +0700

    Check email if exist

--- src/main/java/com/example/arniepanblog/controllers/RegisterController.java
+++ src/main/java/com/example/arniepanblog/controllers/RegisterController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
 @Controller
 public class RegisterController {
 
@@ -23,7 +25,12 @@ public class RegisterController {
     }
 
     @PostMapping("/register")
-    public String registerNewUser(@ModelAttribute Account account) {
+    public String registerNewUser(@ModelAttribute Account account) throws Exception {
+        Optional<Account> accountOptional = accountService.findByEmail(account.getEmail());
+        if (accountOptional.isPresent()) {
+//            return "redirect:/register?error=Email already exists";
+            return "email_exist";
+        }
         accountService.save(account);
         return "redirect:/";
     }
