229813434786710a497671baf23f4c9aabe76195
hoangtv090103 <102798323+hoangtv090103@users.noreply.github.com>
2023-02-26 04:34:35 +0700

    forget password

--- src/main/java/com/example/arniepanblog/controllers/AccountController.java
+++ src/main/java/com/example/arniepanblog/controllers/AccountController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,4 +39,28 @@ public class AccountController {
             return "404";
         }
     }
+
+    @GetMapping("/reset-password")
+    public String getResetPasswordPage(Model model) {
+        Account account = new Account();
+        model.addAttribute("account", account);
+        return "reset_password";
+    }
+
+    @PostMapping("/reset-password")
+    public String resetPassword(@ModelAttribute Account account, @RequestParam("newPassword") String confirmPassword) {
+        Optional<Account> accountOptional = accountService.findByEmail(account.getEmail());
+        if (accountOptional.isPresent()) {
+            if (account.getPassword().equals(confirmPassword)) {
+                Account existingAccount = accountOptional.get();
+                existingAccount.setPassword(account.getPassword());
+                accountService.save(existingAccount);
+                return "redirect:/login";
+            } else {
+                return "password_mismatch";
+            }
+        }
+        return "invalid_email";
+    }
+
 }
